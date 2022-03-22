f2ec3191f47f17aed371b29484509dc849d37a26
James Boyer <jamesboyer92@gmail.com>
2022-03-22 10:18:50 -0400

    added functions to create users and super users, added tests

--- app/core/models.py
+++ app/core/models.py
@@ -10,8 +10,23 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new uer"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates a super user """
+        if not email:
+            raise ValueError('Super Users must have an email address')
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
